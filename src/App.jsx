97bfc4c9c12426d7ca5b55edf991cf1e25af5a8c
Golu <abhisheknamdeo234@gmail.com>
2025-06-01 21:31:33 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Body from './components/Body'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='text-3xl text-red-500'>
-      Hello world bhai
-    </div>
+    <>
+      <Body/>
+    </>
   )
 }
 
